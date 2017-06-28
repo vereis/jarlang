6d11b684b09ec523a0a18db10be1c7b7ddd3fd65
Vereis <chris@thewrongchris.com>
2017-06-28 10:25:57 +0000

    Starting to flesh out the scanning phase of the lexer and trying out a pattern-matching based pattern. We need to decide whether or not to continue with pattern matching or to delegate to one large function with a case statement. The case statement at the moment seems unusual for erlang but we need to look into it.

--- src/jrl_lex.erl
+++ src/jrl_lex.erl
@@ -7,11 +7,33 @@ lex() ->
 	io:format("Usage - lex(Input<String>).~n", []). 
 
 lex(Input) ->
-	Lexemes = re:split(Input, "", [{return, binary}]),
-	scan(Lexemes).	
+	SplitInput = re:split(Input, "", [{return, binary}]),
+	scan(SplitInput).	
 
-scan(Lexemes) ->
-	Lexemes. 
 
+
+
+% Scanner Functions:
+% Scans a list in the form [<<char>...] and generates Lexemes which will be returned 
+scan(Input) ->
+	[Head|Tail] = Input,
+	scan(Tail, []).
+
+scan([], Lexemes) ->
+	Lexemes;
+
+scan([<<"(">>|Tail], Lexemes) ->
+	scan(Tail, Lexemes ++ [paren_l]);
+
+scan([<<")">>|Tail], Lexemes) ->
+	scan(Tail, Lexemes ++ [paren_r]);
+
+scan([_|Tail], Lexemes) ->
+	scan(Tail, Lexemes).
+
+
+
+
+% Lexical Error Reporting
 err({Emsg, Eline}) ->
 	io:format("Error: ~s~nOn line ~s ~n", [Emsg, Eline]).	
