3fba8739f487eac06872ac594cb65b361d8d0910
Andrew <andrew.wnfyc@gmail.com>
2017-10-16 21:09:20 +0100

    Fixed er2file function

--- src/jarlang.erl
+++ src/jarlang.erl
@@ -3,7 +3,8 @@
 
 -module(jarlang).
 -export([er2/2,
-		 er2/3]).
+		 er2file/2,
+		 er2file/3]).
 
 % Transpiles a module through the pipeline as far as is implemented
 er2(best,Module) ->
@@ -20,5 +21,31 @@ er2(core_AST,Module)->
 	{ok,AST}=coregen:er2ast(Module,return_AST),
 	AST.
 
-er2(core_AST,Module,to_file)->
-	coregen:er2ast(Module).
+%############################################
+
+er2file(Task,Module)->
+	code:add_path("../lib/"),
+	OutputDirectory=filepath:path(Module),
+	io:format(OutputDirectory++"~n",[]),
+	er2file(Task,Module,OutputDirectory).
+er2file(Task,Module,OutputDirectory)->
+	toFile(Task,OutputDirectory,Module).
+
+
+toFile(core_erlang,OutputDirectory,Module)->
+	toFile(core_erlang,OutputDirectory,Module,"core");
+toFile(core_AST,OutputDirectory,Module)->
+	toFile(core_AST,OutputDirectory,Module,"ast").
+
+toFile(Task,OutputDirectory,Module,Ext)->
+	code:add_path("../lib/"),
+	ModuleName = filepath:name(Module),
+	Tuple=er2(Task,Module),
+	Str=OutputDirectory ++ ModuleName ++ "." ++ Ext,
+	file:write_file(Str, tuple_to_string(Tuple)).
+
+
+
+% Surprisingly, no tuple_to_string functions exist as a BIF
+tuple_to_string(T) ->
+	lists:flatten(io_lib:format("~p", [T])).
