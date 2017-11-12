4ba1c54ae7d7313692ef18d2e8072d8a36c203ed
osolodo <andrew.wnfyc@gmail.com>
2017-11-12 15:49:29 +0000

    tabs to spaces

--- src/jarlang.erl
+++ src/jarlang.erl
@@ -3,51 +3,51 @@
 
 -module(jarlang).
 -export([er2/2,
-		 er2file/2,
-		 er2file/3]).
+         er2file/2,
+         er2file/3]).
 
 % Transpiles a module through the pipeline as far as is implemented
 
 er2(best,_) ->
-	esast:test(er2(estree,"../doc/Reverse_Engineering_AST/core_AST_revEng"));
+    esast:test(er2(estree,"../doc/Reverse_Engineering_AST/core_AST_revEng"));
 
 er2(core_erlang,Module)->
-	{ok,_,CE}=coregen:er2ce(Module,return_CE),
-	CE;
+    {ok,_,CE}=coregen:er2ce(Module,return_CE),
+    CE;
 
 er2(core_AST,Module)->
-	{ok,AST}=coregen:er2ast(Module,return_AST),
-	AST;
+    {ok,AST}=coregen:er2ast(Module,return_AST),
+    AST;
 
 er2(estree,Module)->
-	AST=er2(core_AST,Module),
-	asttrans:erast2esast(AST).
+    AST=er2(core_AST,Module),
+    asttrans:erast2esast(AST).
 
 
 %############################################
 
 er2file(Task,Module)->
-	code:add_path("../lib/"),
-	OutputDirectory=filepath:path(Module),
-	er2file(Task,Module,OutputDirectory).
+    code:add_path("../lib/"),
+    OutputDirectory=filepath:path(Module),
+    er2file(Task,Module,OutputDirectory).
 er2file(Task,Module,OutputDirectory)->
-	toFile(Task,OutputDirectory,Module).
+    toFile(Task,OutputDirectory,Module).
 
 
 toFile(core_erlang,OutputDirectory,Module)->
-	toFile(core_erlang,OutputDirectory,Module,"core");
+    toFile(core_erlang,OutputDirectory,Module,"core");
 toFile(core_AST,OutputDirectory,Module)->
-	toFile(core_AST,OutputDirectory,Module,"ast").
+    toFile(core_AST,OutputDirectory,Module,"ast").
 
 toFile(Task,OutputDirectory,Module,Ext)->
-	code:add_path("../lib/"),
-	ModuleName = filepath:name(Module),
-	Tuple=er2(Task,Module),
-	Str=OutputDirectory ++ ModuleName ++ "." ++ Ext,
-	file:write_file(Str, tuple_to_string(Tuple)).
+    code:add_path("../lib/"),
+    ModuleName = filepath:name(Module),
+    Tuple=er2(Task,Module),
+    Str=OutputDirectory ++ ModuleName ++ "." ++ Ext,
+    file:write_file(Str, tuple_to_string(Tuple)).
 
 
 
 % Surprisingly, no tuple_to_string functions exist as a BIF
 tuple_to_string(T) ->
-	lists:flatten(io_lib:format("~p", [T])).
+    lists:flatten(io_lib:format("~p", [T])).
