4d305a603a47e3bb64578934dfad5d9344630f5f
nc-cl <ncc.laine@gmail.com>
2017-11-18 01:22:53 +0000

    Add operations to ErlNumber class

--- src/prefabs/datatype_number.js
+++ src/prefabs/datatype_number.js
@@ -2,28 +2,88 @@ var BigNumber = require("bignumber.js");
 
 
 // Constructor
-function ErlNumber(value) {
-    this.value = new BigNumber(value);
+function ErlNumber(val) {
+    this.value = val instanceof BigNumber ? val : new BigNumber(val);
 }
 
 
 // Static Methods
-ErlNumber.isErlNumber = function(erlnum) {
-    return erlnum instanceof ErlNumber;
+ErlNumber.isErlNumber = function(num) {
+    return num instanceof ErlNumber;
 }
 
-ErlNumber.cloneNumber = function(erlnum) {
-    return new ErlNumber(erlnum.toString());
+ErlNumber.cloneNumber = function(num) {
+    return new ErlNumber(num.getValue());
 }
 
 
 // Prototype Methods
+ErlNumber.prototype.getValue = function() {
+    return this.value;
+}
+
 ErlNumber.prototype.toString = function() {
     return this.value.toString();
 }
 
+ErlNumber.prototype.typeOf = function() {
+    return "ErlNumber";
+}
+
+ErlNumber.prototype.isInteger = function() {
+    return this.value.isInteger();
+}
+
+ErlNumber.prototype.isFloat = function () {
+    return !this.value.isInteger();
+}
+
 ErlNumber.prototype.isUnbound = function() {
     return false;
+}
+
+ErlNumber.prototype.add = function(val) {
+    return new ErlNumber(this.value.plus(val instanceof ErlNumber ? val.getValue() : val));
+}
+
+ErlNumber.prototype.subtract = function(val) {
+    return new ErlNumber(this.value.minus(val instanceof ErlNumber ? val.getValue() : val));
+}
+
+ErlNumber.prototype.multiply = function(val) {
+    return new ErlNumber(this.value.times(val instanceof ErlNumber ? val.getValue() : val));
+}
+
+ErlNumber.prototype.divide = function(val) {
+    return new ErlNumber(this.value.div(val instanceof ErlNumber ? val.getValue() : val));
+}
+
+ErlNumber.prototype.intDivide = function(val) {
+    return new ErlNumber(this.value.divToInt(val instanceof ErlNumber ? val.getValue() : val));
+}
+
+ErlNumber.prototype.remainder = function(val) {
+    return new ErlNumber(this.value.mod(val instanceof ErlNumber ? val.getValue() : val));
+}
+
+ErlNumber.prototype.equals = function(val) {
+    return this.value.equals(val);
+}
+
+ErlNumber.prototype.lessThan = function(val) {
+    return this.value.lessThan(val);
+}
+
+ErlNumber.prototype.lessThanOrEq = function(val) {
+    return this.value.lessThanOrEqualTo(val);
+}
+
+ErlNumber.prototype.greaterThan = function(val) {
+    return this.value.greaterThan(val);
+}
+
+ErlNumber.prototype.greaterThanOrEq = function(val) {
+    return this.value.greaterThanOrEqualTo(val);
 }
 
 ErlNumber.prototype.match = function(value) {
