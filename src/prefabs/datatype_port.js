99c6418a071bfeefb27f05b00126ac3f18934e00
osolodo <andrew.wnfyc@gmail.com>
2017-11-19 15:13:01 +0000

    Port datatype stub

---
+++ src/prefabs/datatype_port.js
@@ -0,0 +1,30 @@
+// Constructor
+function Port() {
+    this.value = undefined;
+}
+
+// Static Methods
+Port.isPort = (port) => port instanceof Port;
+
+// Prototype Methods
+Port.prototype.value = function() {
+    throw "Port is not implemented yet";
+}
+
+Port.prototype.toString = function() {
+    throw "Port is not implemented yet";
+}
+
+Port.prototype.isUnbound = function() {
+    return false;
+}
+
+Port.prototype.match = function(X) {
+    // Since 'false' is a legitimate atom in Erlang, we return undefined instead of false for a failure case
+    if (Port.isPort(X) && this.value === X.value) {
+        return X;
+    }
+    else {
+        return undefined;
+    }
+}
