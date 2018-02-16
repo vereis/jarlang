bc5f36ca01f9e8874d4158954a0973be69bb0a23
nc-cl <ncc.laine@gmail.com>
2018-02-16 19:53:54 +0000

    Recommit asttrans changes

--- src/asttrans.erl
+++ src/asttrans.erl
@@ -211,8 +211,10 @@ parse_apply(ReturnAtom, Params, {c_apply, _, {_, _, FunctionName}, Parameters}) 
 %% Parses a c_literal node.
 parse_literal(return, Params, {c_literal, _, Value}) ->
     estree:return_statement(parse_literal(noreturn, Params, {c_literal, [], Value}));
-parse_literal(noreturn, Params, {c_literal, _, Value}) when is_number(Value) ->
-    estree:new_expression(estree:identifier(<<"ErlNumber">>), [estree:literal(Value)]);
+parse_literal(noreturn, Params, {c_literal, _, Value}) when is_integer(Value) ->
+    estree:new_expression(estree:identifier(<<"Int">>), [estree:literal(Value)]);
+parse_literal(noreturn, Params, {c_literal, _, Value}) when is_float(Value) ->
+    estree:new_expression(estree:identifier(<<"Float">>), [estree:literal(Value)]);
 parse_literal(noreturn, Params, {c_literal, _, Value}) when is_atom(Value) ->
     estree:new_expression(estree:identifier(<<"Atom">>), [estree:literal(atom_to_binary(Value, utf8))]);
 
