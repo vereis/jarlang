8f4a7d67593d11a9dc35e4b4fe170ef2053b7698
nc-cl <ncc.laine@gmail.com>
2018-02-20 16:58:35 +0000

    Reintroduce differences in Int datatype

--- src/prefabs/classes/datatype_int.js
+++ src/prefabs/classes/datatype_int.js
@@ -24,37 +24,57 @@ const Int = (() => {
         }
 
         add(val) {
-            return new Int(this.value.plus((val instanceof Int || val instanceof Float) ? val.getValue() : val));
+            if (val instanceof Float) {
+                return new Float(this.value.plus(val.getValue()));
+            }
+            return new Int(this.value.plus(val instanceof Int ? val.getValue() : val);
         }
-        
+
         subtract(val) {
-            return new Int(this.value.minus((val instanceof Int || val instanceof Float) ? val.getValue() : val));
+            if (val instanceof Float) {
+                return new Float(this.value.minus(val.getValue()));
+            }
+            return new Int(this.value.minus(val instanceof Int ? val.getValue() : val);
         }
-        
+
         multiply(val) {
-            return new Int(this.value.times((val instanceof Int || val instanceof Float) ? val.getValue() : val));
+            if (val instanceof Float) {
+                return new Float(this.value.times(val.getValue()));
+            }
+            return new Int(this.value.times(val instanceof Int ? val.getValue() : val);
         }
-        
+
         divide(val) {
-            return new Int(this.value.div((val instanceof Int || val instanceof Float) ? val.getValue() : val));
+            if (val instanceof Float) {
+                return new Float(this.value.div(val.getValue()));
+            }
+            return new Int(this.value.div(val instanceof Int ? val.getValue() : val);
         }
-        
+
+        intDivide(val) {
+            return new Int(this.value.divToInt(val instanceof Int ? val.getValue() : val);
+        }
+
+        remainder() {
+            return new Int(this.value.mod(val instanceof Int ? val.getValue() : val);
+        }
+
         equals(val) {
             return this.value.equals((val instanceof Int || val instanceof Float) ? val.getValue() : val);
         }
-        
+
         lessThan(val) {
             return this.value.lessThan((val instanceof Int || val instanceof Float) ? val.getValue() : val);
         }
-        
+
         lessThanOrEq(val) {
             return this.value.lessThanOrEqualTo((val instanceof Int || val instanceof Float) ? val.getValue() : val);
         }
-        
+
         greaterThan(val) {
             return this.value.greaterThan((val instanceof Int || val instanceof Float) ? val.getValue() : val);
         }
-        
+
         greaterThanOrEq(val) {
             return this.value.greaterThanOrEqualTo((val instanceof Int || val instanceof Float) ? val.getValue() : val);
         }
@@ -62,7 +82,7 @@ const Int = (() => {
         static isInt(a) {
             return a instanceof Int;
         }
-        
+
         static cloneInt(n) {
             return new Int(this.getValue());
         }
