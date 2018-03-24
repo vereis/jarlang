778f186f24ef83dc653aafeb3609261102f618f0
nc-cl <ncc.laine@gmail.com>
2018-03-24 18:44:46 +0000

    Bitstring properly uses size expressions

--- src/js/classes/datatype_bitstring.js
+++ src/js/classes/datatype_bitstring.js
@@ -8,28 +8,57 @@ const BitString = (() => {
             super();
 
             this.precedence = 10;
+            this.value = {
+                sizes: []
+            };
 
             // Process arguments
             const args = [...arguments];
 
-            var tmp = [];
+            var tmp = [], arg;
 
-            args.forEach((arg) => {
-                if (typeof arg[0] == "string" || List.isString(arg[0])) {
-                    arg[0].toString().split("").forEach((c) => tmp.push(c.charCodeAt(0)));
-                }
-                else if (Number.isInteger(arg[0])) {
-                    tmp.push(arg[0]);
+            while (arg = args.shift()) {
+                if (typeof arg == "string" || List.isString(arg)) {
+                    arg.toString().split("").forEach((c) => {
+                        tmp.push(c.charCodeAt(0));
+                        this.value.sizes.push(8);
+                    });
                 }
                 else {
-                    throw `BitString: Bad argument ${arg[0]}`;
-                }
-            });
+                    if (!Array.isArray(arg)) {
+                        arg = [arg];
+                    }
 
-            this.value = {
-                values: Uint8Array.of(...tmp),
-                sizes: [...args].map(x => 1 in x ? x[1] : 8)
-            };
+                    if (!Number.isInteger(arg[0])) {
+                        throw `BitString: Bad argument ${arg[0]}`;
+                    }
+
+                    if (1 in arg && arg[1] !== 8) {
+                        if (arg[1] > 8*2) {
+                            tmp.push(0);
+                            this.value.sizes.push(8);
+                            args.unshift([arg[0], [arg[1] - 8]]);
+                        }
+                        else if (arg[1] > 8) {
+                            tmp.push(arg[0] >> (arg[1] - 8));
+                            this.value.sizes.push(8);
+                            args.unshift([arg[0], arg[1] - 8]);
+                        }
+                        else if (arg[1] >= 1 && arg[1] < 8) {
+                            tmp.push(Math.min(arg[0], (1 << arg[1]) - 1));
+                            this.value.sizes.push(arg[1]);
+                        }
+                        else {
+                            throw `BitString: Bad argument ${arg[1]}`;
+                        }
+                    }
+                    else {
+                        this.value.sizes.push(8);
+                    }
+                }
+            }
+
+            this.value.values = Uint8Array.of(...tmp);
         }
 
         toString() {
@@ -53,7 +82,6 @@ const BitString = (() => {
                     tmp.push(vals.shift());
                 }
             }
-
             return `<<${tmp.join(",")}>>`;
         }
 
